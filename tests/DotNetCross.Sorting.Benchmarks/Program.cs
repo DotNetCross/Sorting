d59449a72e5e9af4fbff4eee48cba091a5756d41
ntr <ntr@ihfood.dk>
2018-01-01 11:55:56 +0100

    add benchmark project, add span sort code from corefx span-sort branch

---
+++ tests/DotNetCross.Sorting.Benchmarks/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+
+namespace DotNetCross.Sorting.Benchmarks
+{
+    [DisassemblyDiagnoser(printAsm: true, printSource: true)]
+    [SimpleJob(RunStrategy.Monitoring, launchCount: 1, warmupCount: 2, targetCount: 3)]
+    [RyuJitX64Job]
+    public class RandomSort
+    {
+        const int Length = 1000000;
+        int[] _random = CreateRandomArray<int>(Length, i => i);
+        int[] _work = new int[Length];
+
+        [IterationSetup]
+        public void IterationSetup() => Array.Copy(_random, _work, Length);
+
+        [Benchmark(Baseline = true)]
+        public void ArraySort()
+        {
+            Array.Sort(_work);
+        }
+
+        [Benchmark]
+        public void SpanSort()
+        {
+            new Span<int>(_work).Sort();
+        }
+
+        const int Seed = 213718398;
+        private static T[] CreateRandomArray<T>(int length, Func<int, T> toValue)
+            where T : IComparable<T>
+        {
+            var random = new Random(Seed);
+            var array = new T[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = toValue(random.Next());
+            }
+            return array;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<RandomSort>();
+        }
+    }
+}
