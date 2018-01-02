db64e8b4aceb5f11765a05e5a3801ff68061ea8f
ntr <ntr@ihfood.dk>
2018-01-02 14:37:08 +0100

    add funny compare benchmark to see weird asm

---
+++ tests/DotNetCross.Sorting.Benchmarks/Micros/CompareAsm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+
+namespace DotNetCross.Sorting.Benchmarks
+{
+    [DisassemblyDiagnoser(printAsm: true, printSource: true, recursiveDepth: 3)]
+    [SimpleJob(launchCount: 1, warmupCount: 2, targetCount: 11)]
+    //[RyuJitX64Job()]
+    public class CompareAsm
+    {
+        ComparableComparer<int> comparer = new ComparableComparer<int>();
+
+        [Params(42)]
+        public int Value { get; set; }
+        [Params(1000)]
+        public int Index { get; set; }
+        [Params(256 * 1024)]
+        public int Length { get; set; }
+        
+        [Benchmark]
+        public int ComparerCompare()
+        {
+            //return Value * Index * Length;
+            unsafe
+            {
+                var a = stackalloc int[Length];
+                a[0] = Value;
+                a[1] = Value + 2;
+                ref var refLoMinus1 = ref a[0];
+                ref var refLo = ref a[1];
+                var i = Index;
+                var n = Length;
+                while (i <= n)
+                {
+                    int child = i << 1;
+                    //if (child < n && comparer(keys[lo + child - 1], keys[lo + child]) < 0)
+                    if (child < n &&
+                        comparer.Compare(Unsafe.Add(ref refLoMinus1, child), Unsafe.Add(ref refLo, child)) < 0)
+                    {
+                        ++child;
+                    }
+
+                    i = child;
+                }
+                return i;
+            }
+        }
+    }
+
+}
--- tests/DotNetCross.Sorting.Benchmarks/Program.cs
+++ tests/DotNetCross.Sorting.Benchmarks/Program.cs
@@ -6,8 +6,9 @@ namespace DotNetCross.Sorting.Benchmarks
     {
         static void Main(string[] args)
         {
-            var s1 = BenchmarkRunner.Run<MedianOfThreeKillerSort>();
-            var s2 = BenchmarkRunner.Run<RandomSort>();
+            //var s1 = BenchmarkRunner.Run<MedianOfThreeKillerSort>();
+            //var s2 = BenchmarkRunner.Run<RandomSort>();
+            var s2 = BenchmarkRunner.Run<CompareAsm>();
 
             //var sut = new RandomSort();
             //for (int i = 0; i < 150; i++)
