7da4fa4dcba9c75280b16307700bf2f543815119
ntr <ntr@ihfood.dk>
2018-01-02 19:44:35 +0100

    minor changes - start on IntPtrHelper

---
+++ src/DotNetCross.Sorting/IntPtrHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace DotNetCross.Sorting
+{
+    public static class IntPtrHelper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe static bool LessThan(this IntPtr a, IntPtr b)
+        {
+            return (sizeof(IntPtr) == sizeof(int))
+                ? (int)a < (int)b
+                : (long)a < (long)b;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe static bool LessThan(this IntPtr a, UIntPtr b)
+        {
+            return (sizeof(UIntPtr) == sizeof(uint))
+                ? (int)a < (int)b
+                : (long)a < (long)b;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe static bool LessThanEqual(this IntPtr a, IntPtr b)
+        {
+            return (sizeof(IntPtr) == sizeof(int))
+                ? (int)a <= (int)b
+                : (long)a <= (long)b;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe static bool LessThanEqual(this IntPtr a, UIntPtr b)
+        {
+            return (sizeof(UIntPtr) == sizeof(uint))
+                ? (int)a <= (int)b
+                : (long)a <= (long)b;
+        }
+    }
+}
--- tests/DotNetCross.Sorting.Benchmarks/Program.cs
+++ tests/DotNetCross.Sorting.Benchmarks/Program.cs
@@ -6,9 +6,9 @@ namespace DotNetCross.Sorting.Benchmarks
     {
         static void Main(string[] args)
         {
-            //var s1 = BenchmarkRunner.Run<MedianOfThreeKillerSort>();
-            //var s2 = BenchmarkRunner.Run<RandomSort>();
-            var s2 = BenchmarkRunner.Run<CompareAsm>();
+            BenchmarkRunner.Run<MedianOfThreeKillerSort>();
+            BenchmarkRunner.Run<RandomSort>();
+            BenchmarkRunner.Run<CompareAsm>();
 
             //var sut = new RandomSort();
             //for (int i = 0; i < 150; i++)
