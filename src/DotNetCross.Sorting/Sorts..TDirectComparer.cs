79e337ec57f8946729064a2f3f2e40c06676e748
ntr <ntr@ihfood.dk>
2020-03-22 14:00:38 +0100

    Separate out HeapSort

---
+++ src/DotNetCross.Sorting/Sorts..TDirectComparer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using static System.SpanSortHelpersCommon;
+
+namespace DotNetCross.Sorting
+{
+    public static partial class Sorts
+    {
+        internal static partial class TDirectComparer
+        {
+        }
+    }
+}
