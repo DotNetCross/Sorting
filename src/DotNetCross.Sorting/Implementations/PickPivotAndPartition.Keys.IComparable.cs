2b68f2c406286b2cefde96837ff51e2e92cd44c1
ntr <ntr@ihfood.dk>
2020-04-27 16:41:26 +0200

    Add new clr code

--- src/DotNetCross.Sorting/Implementations/PickPivotAndPartition.Keys.IComparable.cs
+++ src/DotNetCross.Sorting/Implementations/PickPivotAndPartition.Keys.IComparable.cs
@@ -80,5 +80,64 @@ namespace DotNetCross.Sorting
             }
             return left;
         }
+
+        // Appears Stephen Toub finally added the unsafe version in, which means my original code could have gotten in anyway :|
+        // https://github.com/dotnet/runtime/pull/35297/
+        // https://github.com/dotnet/runtime/blob/8c39b7ec3781af3b3f1b36ce3eeccbb15b3dfb32/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/ArraySortHelper.cs#L453-L499 
+        // ArraySortHelper
+        //private static int PickPivotAndPartition(Span<T> keys)
+        //{
+        //    Debug.Assert(keys.Length >= Array.IntrosortSizeThreshold);
+        //
+        //
+        //    // Use median-of-three to select a pivot. Grab a reference to the 0th, Length-1th, and Length/2th elements, and sort them.
+        //    ref T zeroRef = ref MemoryMarshal.GetReference(keys);
+        //    ref T lastRef = ref Unsafe.Add(ref zeroRef, keys.Length - 1);
+        //    ref T middleRef = ref Unsafe.Add(ref zeroRef, (keys.Length - 1) >> 1);
+        //    SwapIfGreater(ref zeroRef, ref middleRef);
+        //    SwapIfGreater(ref zeroRef, ref lastRef);
+        //    SwapIfGreater(ref middleRef, ref lastRef);
+        //
+        //
+        //    // Select the middle value as the pivot, and move it to be just before the last element.
+        //    ref T nextToLastRef = ref Unsafe.Add(ref zeroRef, keys.Length - 2);
+        //    T pivot = middleRef;
+        //    Swap(ref middleRef, ref nextToLastRef);
+        //
+        //
+        //    // Walk the left and right pointers, swapping elements as necessary, until they cross.
+        //    ref T leftRef = ref zeroRef, rightRef = ref nextToLastRef;
+        //    while (Unsafe.IsAddressLessThan(ref leftRef, ref rightRef))
+        //    {
+        //        if (pivot == null)
+        //        {
+        //            while (Unsafe.IsAddressLessThan(ref leftRef, ref nextToLastRef) && (leftRef = ref Unsafe.Add(ref leftRef, 1)) == null) ;
+        //            while (Unsafe.IsAddressGreaterThan(ref rightRef, ref zeroRef) && (rightRef = ref Unsafe.Add(ref rightRef, -1)) == null) ;
+        //        }
+        //        else
+        //        {
+        //            while (Unsafe.IsAddressLessThan(ref leftRef, ref nextToLastRef) && pivot.CompareTo(leftRef = ref Unsafe.Add(ref leftRef, 1)) > 0) ;
+        //            while (Unsafe.IsAddressGreaterThan(ref rightRef, ref zeroRef) && pivot.CompareTo(rightRef = ref Unsafe.Add(ref rightRef, -1)) < 0) ;
+        //        }
+        //
+        //
+        //        if (!Unsafe.IsAddressLessThan(ref leftRef, ref rightRef))
+        //        {
+        //            break;
+        //        }
+        //
+        //
+        //        Swap(ref leftRef, ref rightRef);
+        //    }
+        //
+        //
+        //    // Put the pivot in the correct location.
+        //    if (!Unsafe.AreSame(ref leftRef, ref nextToLastRef))
+        //    {
+        //        Swap(ref leftRef, ref nextToLastRef);
+        //    }
+        //    return (int)((nint)Unsafe.ByteOffset(ref zeroRef, ref leftRef) / Unsafe.SizeOf<T>());
+        //}
+
     }
 }
