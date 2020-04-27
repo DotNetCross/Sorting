c9aa01ebd459c9b5d8f432837bd6f08bdd208bee
ntr <ntr@ihfood.dk>
2020-04-27 22:09:53 +0200

    insertionsort works too

--- src/DotNetCross.Sorting/Implementations/InsertionSort.Keys.Comparison.cs
+++ src/DotNetCross.Sorting/Implementations/InsertionSort.Keys.Comparison.cs
@@ -6,8 +6,18 @@ namespace DotNetCross.Sorting
 {
     internal static partial class ComparisonImpl
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InsertionSort<TKey>(
+            ref TKey keys, int lo, int hi,
+            Comparison<TKey> comparison)
+        {
+            ref var newStart = ref Unsafe.Add(ref keys, lo);
+            var newLength = hi - lo + 1;
+            //InsertionSortOld(ref keys, lo, hi, comparison);
+            InsertionSort(ref newStart, newLength, comparison);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void InsertionSortOld<TKey>(
              ref TKey keys, int lo, int hi,
              Comparison<TKey> comparison)
         {
@@ -33,5 +43,31 @@ namespace DotNetCross.Sorting
                 }
             }
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void InsertionSort<TKey>(
+             ref TKey keys, int length,
+             Comparison<TKey> comparison)
+        {
+            for (int i = 0; i < length - 1; ++i)
+            {
+                int j = i;
+                ref var keysAtJ = ref Unsafe.Add(ref keys, j);
+                ref var keysAfterJ = ref Unsafe.Add(ref keysAtJ, 1);
+                var t = keysAfterJ;
+                if (comparison(t, keysAtJ) < 0)
+                {
+                    do
+                    {
+                        keysAfterJ = keysAtJ;
+                        keysAfterJ = ref keysAtJ;
+                        keysAtJ = ref Unsafe.Subtract(ref keysAtJ, 1);
+                    }
+                    while (--j >= 0 && comparison(t, keysAtJ) < 0);
+                    keysAfterJ = t;
+                }
+            }
+        }
+
     }
 }
