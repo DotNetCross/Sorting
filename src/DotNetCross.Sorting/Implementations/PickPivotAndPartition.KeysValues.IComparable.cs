5d8bfe0dda8d37ed28bba8092ba51c0ad7720fc8
ntr <ntr@ihfood.dk>
2020-04-27 16:17:26 +0200

    Remove swaps

--- src/DotNetCross.Sorting/Implementations/PickPivotAndPartition.KeysValues.IComparable.cs
+++ src/DotNetCross.Sorting/Implementations/PickPivotAndPartition.KeysValues.IComparable.cs
@@ -86,20 +86,5 @@ namespace DotNetCross.Sorting
             }
             return left;
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static void Swap<T>(ref T items, int i, int j)
-        {
-            Debug.Assert(i != j);
-            Swap(ref Unsafe.Add(ref items, i), ref Unsafe.Add(ref items, j));
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static void Swap<T>(ref T a, ref T b)
-        {
-            var t = a;
-            a = b;
-            b = t;
-        }
     }
 }
